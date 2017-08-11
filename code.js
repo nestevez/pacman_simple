cc5910aea6b6f9723abc7ffe904d38d3f60785d0
Natalia <nataliae.dev@gmail.com>
2017-08-10 20:26:38 -0700

    pacman movements

--- code.js
+++ code.js
@@ -24,6 +24,7 @@ $(document).ready(function(){
   ];
 
   function displayWorld(){
+    $('#screen').html('');
     for (var y = 0; y < map.length; y++){
       for (var x = 0; x < map[y].length; x++) {
         if (map[y][x] == 0){
@@ -42,7 +43,40 @@ $(document).ready(function(){
   displayWorld();
 
   $(document).keydown(function(e){
-    
+    if (e.keyCode == 37){
+      pacman.direction = 'left';
+      if (map[pacman.y][pacman.x-1] != 1){
+        map[pacman.y][pacman.x] = 0;
+        pacman.x-=1;
+        map[pacman.y][pacman.x] = 2;
+      }
+    }
+    if (e.keyCode == 39){
+      pacman.direction = 'right';
+      if (map[pacman.y][pacman.x+1] != 1){
+        map[pacman.y][pacman.x] = 0;
+        pacman.x+=1;
+        map[pacman.y][pacman.x] = 2;
+      }
+    }
+    if (e.keyCode == 38){
+      pacman.direction = 'up';
+      if (map[pacman.y-1][pacman.x] != 1){
+        map[pacman.y][pacman.x] = 0;
+        pacman.y-=1;
+        map[pacman.y][pacman.x] = 2;
+      }
+    }
+    if (e.keyCode == 40){
+      pacman.direction = 'down';
+      if (map[pacman.y+1][pacman.x] != 1){
+        map[pacman.y][pacman.x] = 0;
+        pacman.y+=1;
+        map[pacman.y][pacman.x] = 2;
+      }
+    }
+
+    displayWorld();
   })
 
 })
